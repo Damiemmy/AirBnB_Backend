74b02d0f4c5129fb7dac6bad0153635966df5797
damiemmy <damisaemmanuel778@gmail.com>
2026-02-12 21:37:22 -0800

    created Property django app

--- Airbnb/settings.py
+++ Airbnb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +31,12 @@ DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+AUTH_USER_MODEL='useraccount.User'
+
+SITE_ID=1
+
+WEBSITE_URL='http://localhost:8000'
+
 
 # Application definition
 
@@ -40,11 +47,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+
+    'allauth',
+    'allauth.account',
+    
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+
+    'corsheaders',
+
+    'useraccount',
+    'property'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,6 +93,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Airbnb.wsgi.application'
+
+
 
 
 # Database
@@ -105,7 +130,45 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": True,
+    "SIGNING_KEY": "acomplexkey",
+    "ALGORITHM": "HS512",
+}
 
+ACCOUNT_USER_MODEL_USERNAME_FIELD=None
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_USERNAME_REQUIRED=False
+ACCOUNT_AUTHENTICATION_METHOD="email"
+ACCOUNT_EMAIL_VERIFICATION=None
+
+
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated'
+    )
+}
+
+CORS_ALLOWED_ORIGINS=[
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    
+]
+
+REST_AUTH={
+    "USE_JWT":True,
+    "JWT_AUTH_HTTPONLY": False
+}
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -122,6 +185,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL= 'media/'
+MEDIA_ROOT= BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ property/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+import uuid 
+from useraccount.models import User
+from django.conf import settings
+
+# Create your models here.
+class Property(models.Model):
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title=models.CharField(max_length=225)
+    description=models.TextField()
+    price_per_night=models.TextField()
+    bathroom=models.IntegerField()
+    bedroom=models.IntegerField()
+    guest=models.IntegerField()
+    country=models.CharField(max_length=225)
+    country_code=models.CharField(max_length=10)
+    country=models.CharField(max_length=10)
+    categories=models.CharField(max_length=255)
+
+    #favourite
+    image=models.ImageField(upload_to="uploads/properties")
+    landlord=models.ForeignKey(User, related_name="properties",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def image_url(self):
+        return f"{settings.WEBSITE_URL}{self.image.url}"
+
+
+    
